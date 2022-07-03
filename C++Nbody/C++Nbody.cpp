519d1fd6b2e3f56005787afa208d04a686c1ddfc
Callum <Callumr714@icloud.com>
2022-07-03 18:46:08 +0100

    Public Definitions

--- C++Nbody/C++Nbody.cpp
+++ C++Nbody/C++Nbody.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
 using namespace std;
-int main()
+
+class Nbody
 {
-    cout << "Hello World!" << endl;
-}
+	public:
+		double G = 6.67428e-11;
+		double AU = 149.6e6 * 1000;
+	private:
+
+	
+};
+
